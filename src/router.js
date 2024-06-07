c390d20af3333d2fdb9416d2c3dec2c51cbfe4c9
mayyyy <519030196@@qq.com>
2024-06-07 10:25:51 +0800

    deploy test

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const ENV = process.env.NODE_ENV
 router.get('/api/test', async (ctx, next) => {
     ctx.body = {
         errno: 0,
-        msg: `koa2 test OK , ENV: ${ENV}`
+        msg: `koa2 test OK1 , ENV: ${ENV}`
     }
 })
 
